2e132afa30664e11801d82e58432bacf30c66da6
ServerOnPlate <hayrxdevofficial@gmail.com>
2026-09-18 19:44:25 +0300

    Update MyApp.swift

--- MyApp.swift
+++ MyApp.swift
@@ -1,7 +1,22 @@
 import SwiftUI
+import UIKit
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        return .landscape
+    }
+}
 
 @main
 struct CosmicSungarovApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+    init() {
+        // Отключаем затухание экрана во время игры
+        UIApplication.shared.isIdleTimerDisabled = true
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
